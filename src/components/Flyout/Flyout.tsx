f6cd458ba758b9519007feb5e0bdd94728e90fab
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2024-07-24 12:12:28 +0300

    feat: add dark css style to Flyout component

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -1,18 +1,23 @@
+import { LocalStorageValues } from '../../enums/enums';
 import { selectSelectedPeopleCount } from '../../redux/selectors/selectors';
 import { clearAllSelectedPeople } from '../../redux/slices/mainSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/store/store';
+import { useThemeContext } from '../../utils/constants';
 import styles from './Flyout.module.css';
 
 export default function Flyout() {
   const selectedCount = useAppSelector(selectSelectedPeopleCount());
   const dispatch = useAppDispatch();
+  const { isDark } = useThemeContext();
 
   const clearAll = () => dispatch(clearAllSelectedPeople());
 
   if (!selectedCount) return;
 
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={`${isDark === LocalStorageValues.DARK ? `${styles.wrapper} ${styles.dark}` : `${styles.wrapper}`}`}
+    >
       <p className={styles.text}>
         <span className={styles.text__count}>{selectedCount}</span>
         {selectedCount === 1 ? ' item is ' : ' items are '}selected
