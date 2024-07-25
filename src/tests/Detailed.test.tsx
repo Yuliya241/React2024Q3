3c5d8611bba6b187f4549add5a80ffd7f518ae1a
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2024-07-26 01:12:00 +0300

    fix: fix tests for Detailed component

--- src/tests/Detailed.test.tsx
+++ src/tests/Detailed.test.tsx
@@ -1,10 +1,11 @@
 import { MemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, renderHook, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store/store';
 import Detailed from '../pages/Detailed/Detailed';
-import userEvent from '@testing-library/user-event';
+import { ReactNode } from 'react';
+import { useGetPersonByIdQuery } from '../redux/api/StarWarsApi';
 
 describe('tests for the Detailed component', () => {
   it('displays loading indicator while fetching data', async () => {
@@ -22,31 +23,13 @@ describe('tests for the Detailed component', () => {
     });
   });
 
-  it('the detailed card component correctly displays the detailed card data', async () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['/detailed/62']}>
-          <Detailed />
-        </MemoryRouter>
-      </Provider>
-    );
+  it('renders hook', () => {
+    function Wrapper(props: { children: ReactNode }) {
+      return <Provider store={store}>{props.children}</Provider>;
+    }
 
-    const button = await screen.findByRole('button');
-    expect(button).toHaveTextContent('X');
-  });
-
-  it('clicking the close button hides the component', async () => {
-    const user = userEvent.setup();
-    render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['/detailed/62']}>
-          <Detailed />
-        </MemoryRouter>
-      </Provider>
-    );
-
-    const button = await screen.findByRole('button', { name: 'X' });
-    await user.click(button);
-    expect(button).toBeInTheDocument();
+    renderHook(() => useGetPersonByIdQuery('62'), {
+      wrapper: Wrapper,
+    });
   });
 });
